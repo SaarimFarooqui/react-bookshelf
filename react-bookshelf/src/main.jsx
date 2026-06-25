23f6e6f49788c4ad73a186f30dd09c3b5aefd199
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-25 15:23:59 +0500

    Use usestate to change the gradients of the div. Values change made by clicking on the nav options

--- react-bookshelf/src/main.jsx
+++ react-bookshelf/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState } from 'react'
+import { act, StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 
@@ -7,17 +7,17 @@ const App = () => {
   const [ active_id, setactive_id ] = useState(1);
 
   const navbar_options = [
-    { name : "Bookshelf", id : 1 },
-    { name : "New Book", id : 2},
-    { name : "About", id : 3}
+    { name : "Bookshelf", id : 1, bg: "linear-gradient(45deg, rgb(93, 0, 255) 0%, rgb(0, 170, 255) 50%, rgb(93, 0, 255) 100%)"},
+    { name : "New Book", id : 2, bg: "linear-gradient(45deg, rgb(255, 187, 0) 0%, rgb(30, 255, 0) 50%, rgb(255, 187, 0) 100%)"},
+    { name : "About", id : 3, bg: "linear-gradient(45deg, rgb(0, 0, 0) 0%, rgb(255, 255, 255) 50%, rgb(0, 0, 0) 100%)"}
   ]
-
+  
   return <>
     <div id="background">
       <ul id='nav'>
         <p id="logo">Bookshelf</p>
         { navbar_options.map((option) => 
-          <li 
+          <li key={option.id}
               onClick={ (e) => {
                 e.preventDefault();
                 setactive_id(option.id); } }
@@ -27,7 +27,9 @@ const App = () => {
         }
       </ul>
       <div id="pages-container">
-        <div id="pages"></div>
+        <div id="left-section" style={{background: navbar_options[active_id-1].bg}}>
+        </div>
+        <div id="right-section"></div>
       </div>
 
     </div>
