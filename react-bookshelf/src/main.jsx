b1850422fc8575efa700d72e3983ed29ae5cebb6
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-27 17:40:35 +0500

    Setting up the left section primarily based on text

--- react-bookshelf/src/main.jsx
+++ react-bookshelf/src/main.jsx
@@ -1,6 +1,22 @@
 import { act, StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
+import './text.css'
+
+
+const Bookshelf_left = ({cname}) => {
+  return <div className = {cname}>
+    <p className='text-1'>text-1</p>
+    <p className='text-2'>text-2</p>
+    <p className='text-3'>text-3</p>
+    <p className='text-3'>text-3</p> 
+
+    {/* <p className='text-1'>Welcome user</p>
+    <p className='text-2'>What details are you looking forward to add in your bookshelf today?</p>
+    <p className='text-3'>A new book?</p>
+    <p className='text-3'>Updating progress regarding new book?</p>  */}
+  </div>
+}
 
 const App = () => {
 
@@ -27,8 +43,8 @@ const App = () => {
         }
       </ul>
       <div id="pages-container">
-        <div id="left"></div>
-        <div id="right"></div>
+        <Bookshelf_left cname = "section"/>
+        <div className='section' id="right"></div>
       </div>
 
     </div>
