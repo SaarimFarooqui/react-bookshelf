f098a556437b02ab70e813e900eb36da6fc87750
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-06-28 18:51:15 +0500

    The left section now change the content using active id usestate

--- react-bookshelf/src/main.jsx
+++ react-bookshelf/src/main.jsx
@@ -4,12 +4,45 @@ import './main.css'
 import './text.css'
 
 
-const Bookshelf_left = ({cname}) => {
-  return <div className = {cname}>
+const Bookshelf_left = ({cname, cid}) => {
+  return <div className = {cname} id={cid}>
     <p className='font-1 bgtextclip-1'>Welcome, Dear Reader!</p>
     <p className='font-2 bgtextclip-2'>What details are you looking forward to add in your bookshelf today? A <span className='bgtextclip-3'>New Book</span> or<span className='bgtextclip-1'> updating</span> progress of a previously read book ?</p>
   </div>
 }
+
+const Bookshelf_right = ({cname, cid}) => {
+  return <div className = {cname} id={cid}></div>
+}
+
+const Newbook_left = ({cname, cid}) => {
+  return <div className = {cname} id={cid}>
+    <p className='font-1 bgtextclip-1'>Adding a new Book?</p>
+    <p className='font-2 bgtextclip-2'>Select the right <span className='bgtextclip-3'>category </span>for your book and write its <span className='bgtextclip-1'> Name </span>You can also track the reading progress of your books</p>
+  </div>
+}
+
+const RenderLeft = ({active}) => {
+  switch (active) {
+    case 1: return <Bookshelf_left cname = "section" cid = "left"/>  
+    break;
+    case 2: return <Newbook_left cname = "section" cid = "left"/>  
+    break;
+    default:
+    break;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 const App = () => {
 
@@ -26,8 +59,7 @@ const App = () => {
       <ul id='nav'>
         <p id="logo">Bookshelf</p>
         { navbar_options.map((option) => 
-          <li key={option.id}
-              onClick={ (e) => {
+        <li key={option.id} onClick={ (e) => {
                 e.preventDefault();
                 setactive_id(option.id); } }
               className={option.id === active_id ? "active-li" : null}>
@@ -36,8 +68,8 @@ const App = () => {
         }
       </ul>
       <div id="pages-container">
-        <Bookshelf_left cname = "section"/>
-        <div className='section' id="right"></div>
+        { <RenderLeft active={active_id}/>}
+        <Bookshelf_right cname = "section" cid = "right"/>
       </div>
 
     </div>
