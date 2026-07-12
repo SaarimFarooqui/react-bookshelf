5c56a8cf48193cfa4895fcf97d221e9d6ee7cf06
Saarim Farooqui <saarimfarooqui000@gmail.com>
2026-07-12 09:21:04 +0500

    Blob gradient screen background

---
+++ react-bookshelf/src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+import "../css/landingpage.css";
+export default function LandingPage() {
+    return(
+        <div className="lp-bg">
+            <div className="gradientLight-1"></div>
+            <div className="gradientLight-2"></div>
+        </div>
+    )
+}
